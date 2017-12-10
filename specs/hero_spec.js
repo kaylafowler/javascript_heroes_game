70885436e43496b775abb27992b4548b9c21d675
SwayGatsby <kayla.fowler@hotmail.com>
2017-12-10 19:04:13 +0000

    tests all passed for Hero constructor

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -16,6 +16,20 @@ describe("Hero", function(){
     assert.strictEqual(theTick.name, "The Tick");
   });
 
+  it("should have health that starts at 100", function(){
+    assert.strictEqual(arthur.health, 100);
+  });
 
+  it("should have a favourite food", function(){
+    assert.strictEqual(theTick.favFood, "hot dogs")
+  });
+
+  it("should be able to speak", function(){
+    assert.strictEqual(theTick.speak(), "Do not fear, The Tick is here!")
+  });
+
+  it("should have a collection of tasks to complete, which starts empty", function(){
+    assert.strictEqual(arthur.tasks.length, 0)
+  });
 
 });
